61531df9f890b095893314034f52287e36fa1589
Namrata Jaiswal <106535364+NamrataCodeCraft@users.noreply.github.com>
2024-06-06 19:32:38 +0530

    worked on login and forgot password api

--- src/config/config.js
+++ src/config/config.js
@@ -4,8 +4,8 @@ require("dotenv").config({ path: "../../.env" });
 module.exports = {
   development: {
     username: process.env.DB_USERNAME || "postgres",
-    password: process.env.DB_PASSWORD || "test@123",
-    database: process.env.DB_NAME || "hrms_backend",
+    password: process.env.DB_PASSWORD || "1234",
+    database: process.env.DB_NAME || "HRMS_TEST",
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || "5432",
     dialect: 'postgres',
