a0a142bff71ab798e6573846f91e3ff5142428de
nayaka707 <hnayaka@pragetx.com>
2024-06-13 18:07:48 +0530

    Changes in seeders files

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,20 @@
 require("dotenv").config({ path: "../../.env" });
+
+// const username = process.env.DB_USERNAME || "postgres";
+// const password = process.env.DB_PASSWORD || "root";
+// const database = process.env.DB_NAME || "hrms_backend";
+// const host = process.env.DB_HOST || "localhost";
+// const port = process.env.DB_PORT || 5432 ;
+
+
+
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || "postgres",
-    password: process.env.DB_PASSWORD || "test@123",
-    database: process.env.DB_NAME || "hrms_backend",
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || "5432",
+    username: process.env.DB_USERNAME ,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME ,
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT,
     dialect: "postgres",
   },
 };
