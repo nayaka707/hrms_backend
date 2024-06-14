0bbf574d28e66c0a96ae7715970a73cf639fbd07
Namrata Jaiswal <106535364+NamrataCodeCraft@users.noreply.github.com>
2024-06-14 18:56:27 +0530

    removed config details

--- src/config/config.js
+++ src/config/config.js
@@ -1,41 +1,14 @@
-// require("dotenv").config({ path: "../../.env" });
+require("dotenv").config({ path: "../../.env" });
 
-// const username = process.env.DB_USERNAME || "postgres";
-// const password = process.env.DB_PASSWORD || "root";
-// const database = process.env.DB_NAME || "hrms_backend";
-// const host = process.env.DB_HOST || "localhost";
-// const port = process.env.DB_PORT || 5432 ;
-
-
-
-// module.exports = {
-//   development: {
-//     username: process.env.DB_USERNAME ,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME ,
-//     host: process.env.DB_HOST ,
-//     port: process.env.DB_PORT,
-//     dialect: "postgres",
-//   },
-// };
-
-
-require("dotenv").config({ path: '../../.env' });
-// require("dotenv").config();
-
-console.log('DB_USERNAME:', process.env.DB_USERNAME);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_PORT:', process.env.DB_PORT);
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME || "postgres",
-    password: process.env.DB_PASSWORD || "1234",
-    database: process.env.DB_NAME || "HRMS",
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || 5432,
-    dialect: 'postgres',
+    username: process.env.DB_USERNAME ,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME ,
+    host: process.env.DB_HOST ,
+    port: process.env.DB_PORT,
+    dialect: "postgres",
   },
 };
+
