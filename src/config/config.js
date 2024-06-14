195fd9130a6cd422a807c7260ef780a629614bee
Namrata Jaiswal <106535364+NamrataCodeCraft@users.noreply.github.com>
2024-06-14 18:50:32 +0530

    1) created new table named EmployeeLogDetails and added colums like log_date, direction, employee_code etc 2) API of creating EmployeeLogDetails and getEmployeeLogDetails 3.) added colums branchName and employee_code into employee 4.) set employee_code into token 5.) added migration to add  emergencyContact, city, state, picode etc. colums into employee

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "../../.env" });
+// require("dotenv").config({ path: "../../.env" });
 
 // const username = process.env.DB_USERNAME || "postgres";
 // const password = process.env.DB_PASSWORD || "root";
@@ -8,13 +8,34 @@ require("dotenv").config({ path: "../../.env" });
 
 
 
+// module.exports = {
+//   development: {
+//     username: process.env.DB_USERNAME ,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME ,
+//     host: process.env.DB_HOST ,
+//     port: process.env.DB_PORT,
+//     dialect: "postgres",
+//   },
+// };
+
+
+require("dotenv").config({ path: '../../.env' });
+// require("dotenv").config();
+
+console.log('DB_USERNAME:', process.env.DB_USERNAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME ,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME ,
-    host: process.env.DB_HOST ,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
+    username: process.env.DB_USERNAME || "postgres",
+    password: process.env.DB_PASSWORD || "1234",
+    database: process.env.DB_NAME || "HRMS",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 5432,
+    dialect: 'postgres',
   },
 };
